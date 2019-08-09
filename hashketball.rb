79246904ecaaa588c7efd728937a2e6899668eb6
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-09 18:31:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -239,6 +239,7 @@ def winning_team
   home_team_points = 0 
   
   game_hash[:away][:players].each do |player, attribute |
+    binding.pry 
     away_team_points = away_team_points + attribute[:points]
       game_hash[:home][:players].each do |player, attribute|
         home_team_points = home_team_points + attribute[:points]
